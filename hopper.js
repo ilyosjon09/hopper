2dcd21a313ac47b8222b7c99c5dc4b2eb7e9c876
Ilyosjon Kamoldinov <ilyosjon09@gmail.com>
2020-04-30 08:33:33 +0500

    Send proper messages between scripts

--- hopper.js
+++ hopper.js
@@ -1,11 +1,19 @@
 let inputs = document.querySelectorAll("input");
 let activeInput = {};
+let counter = 1;
 
 inputs.forEach(function (input) {
   input.addEventListener("focus", function(e) {
-      activeInput = e.target;
-      browser.runtime.sendMessage({
-          tabindex: e.target.getAttribute("tabindex")
-        });
+    e.target.setAttribute("data-hopperid", "hop" + counter);
+    counter++;
+    browser.runtime.sendMessage({
+      tabindex: e.target.getAttribute("tabindex"),
+      hopperId: e.target.dataset.hopperid
+    });
   });
 });
+
+browser.runtime.onMessage.addListener(alertHello);
+function alertHello(message) {
+  console.log(message);
+}
--- sidebar.js
+++ sidebar.js
@@ -1,9 +1,20 @@
-browser.windows.getCurrent({populate: true}).then((windowInfo) => {
-    myWindowId = windowInfo.id;
-});
 let activeInput = document.querySelector("#activeInput");
+let btnSave = document.querySelector("#btnSave");
+let hopperId = "";
+
 browser.runtime.onMessage.addListener(getTabIndex);
 
 function getTabIndex(message) {
-activeInput.value = message.tabindex;
+    if(message.hasOwnProperty("tabindex")) {
+        activeInput.value = message.tabindex;
+        hopperId = message.hopperId;
+    }
+}
+
+btnSave.addEventListener("click", sendMessage);
+
+function sendMessage() {
+    browser.tabs.query({ currentWindow: true, active: true }).then( tabs => {
+        browser.tabs.sendMessage(tabs[0].id, { hopperId: hopperId,tabindex: activeInput.value });
+    }).catch(errorData => console.log(errorData));
 }
